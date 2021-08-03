a3dc41b361608ae820e6464f7c15742cf05e3e2e
abattistini <alberto.battistini@soprasteria.com>
2021-08-03 11:07:42 +0200

    ugly number

---
+++ src/main/java/leetcode/UglyNumber.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+/**
+ * An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+ *
+ * Given an integer n, return true if n is an ugly number.
+ *
+ * @author Alberto Battistini
+ * @version 0.1
+ * @since 0.1
+ */
+public class UglyNumber {
+
+    static boolean isUgly(int n) {
+
+        if (n < 1)
+            return false;
+
+        n = divideBy(n, 2);
+        n = divideBy(n, 3);
+        n = divideBy(n, 5);
+
+        return n == 1;
+    }
+
+    private static int divideBy(int n, int div) {
+        while (n % div == 0) {
+            n = n / div;
+        }
+        return n;
+    }
+
+    public static void main(String[] args) {
+        int n = 8;
+        System.out.println(isUgly(10));
+    }
+}
