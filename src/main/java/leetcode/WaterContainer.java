1b43834b1c4eea9c96ca6f912fe7a3eae8ed65b0
Alberto B <battistinimail@gmail.com>
2021-08-04 13:21:54 +0200

    water container: to fix

---
+++ src/main/java/leetcode/WaterContainer.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+public class WaterContainer {
+
+    static int maxArea(int[] height) {
+        int n = height.length;
+        int max = 0;
+        int area;
+        int maxSum = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            for (int j = i + 1; j < n; j++) {
+
+                int sum = Math.min(height[j], height[i]) + (j - i);
+                if (sum < maxSum)
+                    continue;
+                maxSum = sum;
+
+                if (height[j] <= height[i]) {
+                    area = height[j] * (j - i);
+                }
+                else {
+                    area = height[i] * (j - i);
+                }
+                if (area > max)
+                    max = area;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        int[] height = new int[] {136,114,104,156,183,152,66,189,116,94,90,58,10,131,67,85,183,174,174,135,88,79,19,0,15,28,96,95,35,174,38,171,88,142,80,24,95,146,13,163,192,103,21,154,187,40,40,122,167,166,58,7,46,77,7,13,58,56,109,93,182,99,17,23,194,97,47,89,195,12,4,188,68,26,142,7,18,134,129,185,101,187,144,99,17,104,112,75,160,173,120,142,73,89,165,67,138,164,108,134};
+        System.out.println(maxArea(height));
+        System.out.println("Expected: 15252");
+    }
+}
