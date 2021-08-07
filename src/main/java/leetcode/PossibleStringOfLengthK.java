211d840e22dae0ee05293dde1a3ec8a67caa808a
abattistini <alberto.battistini@soprasteria.com>
2021-08-07 20:05:08 +0200

    print all strings of k length of values n

--- src/main/java/leetcode/PossibleStringOfLengthK.java
+++ src/main/java/leetcode/PossibleStringOfLengthK.java
@@ -1,4 +1,27 @@
 package leetcode;
 
 public class PossibleStringOfLengthK {
+
+    static void printAllStringOfKLength(int n, int k) {
+        if (k == 0)
+            return;
+        print_rec("", n, k);
+    }
+
+    static void print_rec(String prefix, int n, int k) {
+        if (k == 0) {
+            System.out.println(prefix);
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            String new_prefix = prefix + i;
+            print_rec(new_prefix, n, k-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int k = 3;
+        int n = 2; // values 0 - 1
+        printAllStringOfKLength(n, k);
+    }
 }
